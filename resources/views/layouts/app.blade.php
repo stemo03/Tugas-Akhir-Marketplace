642a65ec2d53357369af0aa578899d21d44e70c6
Stevi <stevimonicatarigan03@gmail.com>
2021-07-20 21:37:13 +0700

    app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                 <!-- section intro row ends --> 
                 
                 <!-- row 1 starts -->
-                <div class="row"  data-aos="fade-up"
+                {{-- <div class="row"  data-aos="fade-up"
                         data-aos-delay="100">
                     <div class="col-12 col-md-2 offset-md-1 text-center dtr-pt-100 dtr-mt-120 dtr-md-mt-0 dtr-md-pt-0"> <img src="images\admin.png" alt="image" class="dtr-mb-30 w-25">
                         <h6>If you feel unwell get medical care early</h6>
@@ -53,7 +53,7 @@
                     <div class="col-12 col-md-2 text-center dtr-pt-100 dtr-mt-120 dtr-md-mt-30 dtr-md-pt-0"> <img src="images\admin.png" alt="image" class="dtr-mb-30 w-25">
                         <h6>Avoid touching eyes, nose and mouth</h6>
                     </div>
-                </div>
+                </div> --}}
 {{-- <div class="container " data-aos="zoom-in"
                         data-aos-delay="200">
        <div class="my-jumbotron jumbotron jumbotron-fluid">
